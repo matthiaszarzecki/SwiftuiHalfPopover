a048654643e68ff3bb5af9313572c07ca32ecac5
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2020-11-09 18:25:12 +0100

    Extract view, improve formatting

--- SlidingOverlayTest/SlidingOverlayTest/ContentView.swift
+++ SlidingOverlayTest/SlidingOverlayTest/ContentView.swift
@@ -22,35 +22,49 @@ struct ContentView: View {
         }
         
         if showDetails {
-          VStack {
-            // The upper "Empty" part of the view
-            Spacer()
-          
-            // The actual popover
-            ZStack {
-              Rectangle()
-                .foregroundColor(.blue)
-                .cornerRadius(24)
-                .frame(width: geometry.size.width, height: geometry.size.height * 0.4, alignment: .center)
-                
-              VStack {
-                Text("Overlay!")
-                  .transition(.move(edge: .bottom))
-                
-                Button(action: {
-                  withAnimation {
-                    self.showDetails.toggle()
-                  }
-                }) {
-                  Text("Close")
-                    .foregroundColor(.white)
-                }
-              }
-            }
-          }
-          .transition(.move(edge: .bottom))
+          HalfPopover(
+            width: geometry.size.width,
+            height: geometry.size.height,
+            showDetails: $showDetails
+          )
         }
       }
+    }
+  }
+  
+  struct HalfPopover: View {
+    var width: CGFloat
+    var height: CGFloat
+    @Binding var showDetails: Bool
+  
+    var body: some View {
+      VStack {
+        // The upper "Empty" part of the view
+        Spacer()
+      
+        // The actual popover
+        ZStack {
+          Rectangle()
+            .foregroundColor(.blue)
+            .cornerRadius(24)
+            .frame(width: width, height: height * 0.4, alignment: .center)
+            
+          VStack {
+            Text("Overlay!")
+              .transition(.move(edge: .bottom))
+            
+            Button(action: {
+              withAnimation {
+                showDetails.toggle()
+              }
+            }) {
+              Text("Close")
+                .foregroundColor(.white)
+            }
+          }
+        }
+      }
+      .transition(.move(edge: .bottom))
     }
   }
 }
